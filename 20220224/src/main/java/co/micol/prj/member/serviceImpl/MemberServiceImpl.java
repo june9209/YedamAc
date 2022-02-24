6166d66ba5432cd9d9e22a8bd424e34cd846025f
June <qk1413@naver.com>
2022-02-24 12:51:10 +0900

    12

--- 20220224/src/main/java/co/micol/prj/member/serviceImpl/MemberServiceImpl.java
+++ 20220224/src/main/java/co/micol/prj/member/serviceImpl/MemberServiceImpl.java
@@ -3,6 +3,8 @@ package co.micol.prj.member.serviceImpl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import co.micol.prj.common.DataSource;
@@ -17,37 +19,124 @@ public class MemberServiceImpl implements MemberService {
 	@Override
 	public List<MemberVO> memberSelectList() {
 		// 전체리스트 가져오기
-		return null;
+		List<MemberVO> members = new ArrayList<MemberVO>();
+		MemberVO vo; 
+		String sql = "SELECT * FROM MEMBER";
+		try {
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			while(rs.next()) {
+				vo = new MemberVO();
+				vo.setId(rs.getString("id"));
+				vo.setName(rs.getString("name"));
+				vo.setTel(rs.getString("tel"));
+				vo.setAddress(rs.getString("address"));
+				vo.setAuthor(rs.getString("author"));
+				members.add(vo);
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return members;
+	}
+
+	private void close() {
+		//Connection 연결 종료
+		try {
+			if(rs != null)rs.close();
+			if(psmt != null)psmt.close();
+			if(conn != null)conn.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public MemberVO memberSelect(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+		//한명분 조회
+		String sql = "select * from member where id = ?";
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1,vo.getId());
+			rs = psmt.executeQuery();
+			while(rs.next()) {
+				vo = new MemberVO();
+				vo.setId(rs.getString("id"));
+				vo.setPassword(rs.getString("password"));
+				vo.setName(rs.getString("name"));
+				vo.setTel(rs.getString("tel"));
+				vo.setAddress(rs.getString("address"));
+				vo.setAuthor(rs.getString("author"));
+				
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return vo;
 	}
 
 	@Override
 	public int memberInsert(MemberVO vo) {
-		// TODO Auto-generated method stub
+		// 멤버 추가
+		String sql = "INSERT INTO MEMBER VALEUS(?,?,?,?,?,?,?)";
 		return 0;
 	}
 
 	@Override
 	public int memberUpdate(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "update member set password =? , tel=?, address= ? , author=?" + " where = ? ";
+		int n = 0;
+		try {
+			psmt = conn.prepareStatement(sql);
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return n;
 	}
 
 	@Override
 	public int memberDelete(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "DELETE FROM MEMBER WHERE ID = ?";
+		int n = 0;
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1,  vo.getId());
+			n = psmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return n;
 	}
 
 	@Override
 	public boolean isIdcheck(String id) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql = "select case when count(id) = 1 then 0 else 1 end as id"
+				+ " from member where id = ?";
+		boolean b = false;
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+			rs = psmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getInt("id") != 0) {
+					b = true;
+				}
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return b;
 	}
 
 	@Override
